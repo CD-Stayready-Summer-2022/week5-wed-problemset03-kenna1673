2a59e5d504b71cc111c4037b7ef06d9a2b549a4d
McKenna <ohara10@purdue.edu>
2022-07-06 14:24:36 -0400

    finished 2

--- src/main/java/com/codedifferently/problems/Problem02.java
+++ src/main/java/com/codedifferently/problems/Problem02.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class Problem02 {
     public static Integer cousinTag(ArrayList<String> cousins, Integer startIndex, String targetName){
-        return null;
+        int indexOfTarget = cousins.indexOf(targetName);
+        int wrapped = startIndex - cousins.size();
+        int regularDifference = Math.abs(startIndex - indexOfTarget);
+        int wrappedDifference = Math.abs(indexOfTarget - wrapped);
+        return Math.min(regularDifference, wrappedDifference);
     }
 }
