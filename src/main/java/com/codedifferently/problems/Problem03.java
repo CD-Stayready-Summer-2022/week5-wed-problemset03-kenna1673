1ce962b654dd7e52301719b1feda8fc26448e785
McKenna <ohara10@purdue.edu>
2022-07-06 15:00:16 -0400

    done 3:00

--- src/main/java/com/codedifferently/problems/Problem03.java
+++ src/main/java/com/codedifferently/problems/Problem03.java
@@ -1,5 +1,9 @@
 package com.codedifferently.problems;
 
+import javax.print.attribute.IntegerSyntax;
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Problem03 {
     /**
      * You will be given an integer called number and an array called possibleFamilyMembers
@@ -18,6 +22,44 @@ public class Problem03 {
      * @return
      */
     public Integer[] numberFamily(Integer number, Integer[] possibleFamilyMembers){
-        return null;
+        /*List<Integer> familyMembers = new ArrayList<>();
+        for (Integer num : possibleFamilyMembers) {
+            if (Math.abs(num - number) <= 1) {
+                familyMembers.add(num);
+            }
+        }
+        Integer[] array = new Integer[familyMembers.size()];
+        array = familyMembers.toArray(array);
+        return array;
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (Integer num : possibleFamilyMembers) {
+            map.put(num, new ArrayList<>());
+            for (int i = 1; i < possibleFamilyMembers.length; i++) {
+                if (Math.abs(num - number) <= 1) {
+                    map.put(num, )
+                }
+            }
+        }*/
+        List<Integer> familyMembers = new ArrayList<>();
+        Arrays.sort(possibleFamilyMembers);
+        List<Integer> nums = new ArrayList<>(Arrays.asList(possibleFamilyMembers));
+        int start = nums.indexOf(number);
+        for (int i = start - 1; i < possibleFamilyMembers.length - 1; i++) {
+            if (Math.abs(possibleFamilyMembers[i] - possibleFamilyMembers[i + 1]) <= 1) {
+                familyMembers.add(possibleFamilyMembers[i + 1]);
+            }
+        }
+
+        for (int i = start; i > 0; i--) {
+            if (Math.abs(possibleFamilyMembers[i] - possibleFamilyMembers[i - 1]) <= 1) {
+                familyMembers.add(possibleFamilyMembers[i - 1]);
+                continue;
+            }
+            break;
+        }
+        Integer[] array = new Integer[familyMembers.size()];
+        array = familyMembers.toArray(array);
+        Arrays.sort(array);
+        return array;
     }
 }
